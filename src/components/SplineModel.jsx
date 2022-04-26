63a9d117d664cc398198f983e92ea35934223378
TineoGilbert <65523573+TineoGilbert@users.noreply.github.com>
2022-04-26 19:36:12 -0400

    Spline 3D Model - React

---
+++ src/components/SplineModel.jsx
@@ -0,0 +1,13 @@
+import React from 'react'
+import Spline from '@splinetool/react-spline'
+
+const SplineModel = () => {
+  return (
+    <Spline scene="https://draft.spline.design/yr-RXIZN5YvqyD8d/scene.spline" />
+    
+  )
+}
+
+export default SplineModel
+
+
---
+++ src/main.jsx
@@ -0,0 +1,14 @@
+import React from 'react'
+import ReactDOM from 'react-dom/'
+import SplineModel from './components/SplineModel'
+import './index.css'
+
+/*
+ReactDOM.createRoot(document.getElementById('root')).render(
+  <React.StrictMode>
+    <App />
+  </React.StrictMode>
+)
+*/
+
+ReactDOM.render(<SplineModel/>, document.getElementById('root'))
